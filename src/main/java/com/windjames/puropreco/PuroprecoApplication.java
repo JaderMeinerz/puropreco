f4cc8a0d7b60cad6cf678bbc8c744e3eea6095b3
JaderMeinerz <jadermeinerzdossantos@yahoo.com.br>
2023-05-18 15:57:14 -0300

    first commit

---
+++ src/main/java/com/windjames/puropreco/PuroprecoApplication.java
@@ -0,0 +1,13 @@
+package com.windjames.puropreco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuroprecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuroprecoApplication.class, args);
+	}
+
+}
